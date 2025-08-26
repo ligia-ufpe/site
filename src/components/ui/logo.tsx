d906128a2ff044d6c0a1ddace6afcbb8d7f77835
Rodrigo Rossiter Guimarães Filho <rfilho@informa.com.br>
2025-08-26 13:32:58 -0300

    fix(logo): update header/footer logo image source and adjust width

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -4,11 +4,10 @@ export function Logo() {
   return (
     <Link to="/" className="flex items-center gap-2">
       <img 
-        src="images/logo.svg" 
+        src="images/logo+nome.svg" 
         alt="Ligia Logo" 
-        className="w-8 h-8 relative"
+        className="w-25 h-8 relative"
       />
-      <span className="text-2xl font-bold text-white">Ligia</span>
     </Link>
   );
 }
